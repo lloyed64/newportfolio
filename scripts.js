cec49389af57d996165ec82a0ae03f76c0d38329
Lloyed <lloyedcho@gmail.com>
2020-02-04 05:20:54 -0700

    update

--- scripts.js
+++ scripts.js
@@ -36,7 +36,7 @@ function topFunction() {
 }
 
 //scroll down button
-$(scrolldown function() {
+$(function() {
   $('a[href*=#]').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
